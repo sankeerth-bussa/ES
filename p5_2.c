f6d89e75b96ad97e2ddba3e50db0d6a3bd22f9f3
Sankeerth Bussa <sankeerthbussa@gmail.com>
2021-11-28 23:04:20 +0530

    Create p5_2.c

---
+++ p5_2.c
@@ -0,0 +1,23 @@
+#include <LPC214x.h>
+#define DAC_BIAS 0
+void DacInit( void );
+void DacOut( unsigned int val );
+void Delay45Us(void);
+int main (void){
+unsigned int i = 0;
+DacInit();
+IODIR1|=0xFFFF0000;
+IOPIN1&=~(0xFFFF0000);
+while (1) {
+for(i=0;i<1024;i++)
+DacOut(i);
+for(i=1022;i>0;i--)
+DacOut(i);
+}
+}
+void DacInit( void ) {
+PINSEL1 |= 0x00080000;
+}
+void DacOut( unsigned int val ) {
+DACR = (val << 6) | (DAC_BIAS<<16);
+}
